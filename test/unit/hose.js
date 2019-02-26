4e4459cb75c249c2624ea10409bf7fe2a02a3353
freemany <freeman_yam@hotmail.com>
2019-02-27 01:00:00 +1100

    WIP

--- test/unit/hose.js
+++ test/unit/hose.js
@@ -3,67 +3,85 @@ const spy = {result: 0};
 
 describe('Hose', () => {
 
-    it('no yield no call', () => {
-        // 10 + 1 - 2  = 9
-        (new Hose(10))
-            .pipe((res) => {
-               spy.result ++;
-               resolve(res + 1);
-        }).pipe(res => res - 2);
+    // it('no yield no call', () => {
+    //     // 10 + 1 - 2  = 9
+    //     (new Hose(10))
+    //         .pipe((res) => {
+    //            spy.result ++;
+    //            resolve(res + 1);
+    //     }).pipe(res => res - 2);
+    //
+    //     expect(spy.result).to.equal(0);
+    // });
 
-        expect(spy.result).to.equal(0);
-    });
-
-    it('yield', () => {
+    // it('yield', (done) => {
+    //     // 10 + 1 - 2  = 9
+    //     (new Hose(10))
+    //         .pipe((res, resolve) => {
+    //             spy.result ++;
+    //             resolve(res + 1);
+    //         })
+    //         .pipe((res, resolve) => {
+    //             resolve(res - 2);
+    //             spy.result ++;
+    //          })
+    //         .yield((res) => {
+    //             expect(res).to.equal(9);
+    //             expect(spy.result).to.equal(2);
+    //             done();
+    //         });
+    // });
+    //
+    it('yield first', (done) => {
         // 10 + 1 - 2  = 9
-        (new Hose(10))
+        let h = (new Hose(10))
             .pipe((res, resolve) => {
-                spy.result ++;
                 resolve(res + 1);
+                expect(res).to.equal(10);
+                done()
+            }).pipe((res, resolve) => {
+                resolve(res -2 );
             })
-            .pipe((res, resolve) => {
-                resolve(res - 2);
-                spy.result ++;
-             })
             .yield((res) => {
-                expect(res).to.equal(9);
-                expect(spy.result).to.equal(2);
             });
     });
 
-    it('yield invalid', () => {
-        // 10 + 1 - 2  = 9
-        (new Hose(10))
-            .pipe((res, resolve) => {
-                resolve(res + 1);
-            })
-            .pipe((res, resolve) => {
-                resolve(res - 2)
-            })
-            .yield((res) => {
-                expect(res).not.to.equal(19);
-            });
-    });
+    // it('yield first', (done) => {
+    //     // 10 + 1 - 2  = 9
+    //     (new Hose(10))
+    //         .pipe((res, resolve) => {
+    //             setTimeout((res) => {
+    //                 resolve(res + 1);
+    //             }, 0);
+    //         }).pipe((res, resolve) => {
+    //             resolve(res -2 );
+    //             expect(res).to.equal(11);
+    //             done();
+    //         })
+    //         .yield((res) => {
+    //         });
+    // });
 
-    it('async yield', async () => {
+    it('async yield', (done) => {
         // 10 * 4 - 20  = 20
         (new Hose(10))
-            .pipe(async(res, resolve) => {
+            .pipe((res, resolve) => {
                 setTimeout(() => {
                     resolve(res * 4);
-                }, 1);
+                }, 0);
             })
-            .pipe(async (res, resolve) => {
+            .pipe((res, resolve) => {
                 setTimeout(() => {
                     resolve(res - 20);
-                }, 2);
+                }, 1);
             })
             .yield((res) => {
                 expect(res).to.equal(20);
+                done();
             });
     });
 
-    it('async yield invalid', async () => {
+    it('async yield invalid', (done) => {
         // 100 + 10 - 20  = 90
         (new Hose(100))
             .pipe((res, resolve) => {
@@ -77,7 +95,8 @@ describe('Hose', () => {
                 }, 2);
             })
             .yield((res) => {
-                expect(res).not.to.equal(900);
+                expect(res).to.not.equal(90 + Math.random());
+                done();
             });
     });
 });
